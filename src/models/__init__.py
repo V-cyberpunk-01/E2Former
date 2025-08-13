6ff71b71813efbf00a5bef9627a80c00636ef063
yl2428 <yunyang.li@yale.edu>
2025-08-13 15:18:12 -0400

    Refactor E2Former model imports and remove modular file
    - Updated __init__.py to import get_powers from e2former_main and added it to the __all__ list.
    - Deleted the e2former_modular.py file as its functionality is now integrated into other modules for improved organization.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,9 +4,8 @@ E2Former Models Module
 Contains the main model implementations.
 """
 
-from .e2former_main import E2former
+from .e2former_main import E2former, get_powers
 from .e2former_cluster import E2formerCluster, E2AttentionArbOrder_sparse_forcluster
-from .e2former_modular import *
 from .E2Former_wrapper import E2FormerBackbone
 
 __all__ = [
@@ -14,4 +13,5 @@ __all__ = [
     "E2formerCluster",
     "E2AttentionArbOrder_sparse_forcluster",
     "E2FormerBackbone",
+    "get_powers",
 ]
